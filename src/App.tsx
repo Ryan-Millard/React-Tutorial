605e35a592cc3e3343f7461ad0404e54f6a4dada
Ryan Millard <millardryandevon@gmail.com>
2024-02-07 19:05:22 +0200

    Added UseEffectExample to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import UseStateExample from './Hooks/useState.tsx';
+import UseEffectExample from './Hooks/useEffect.tsx';
 
 function App() {
 	return (
-	<>
-		<div>
-			<UseStateExample />
-		</div>
-	</>
+		<>
+			<div>
+				{/* <UseStateExample /> */}
+				<UseEffectExample />
+			</div>
+		</>
 	);
 }
 
-export default App
+export default App;
