ed52806a3e780e5b8540f943cca38bfa86345ef4
Ryan Millard <millardryandevon@gmail.com>
2024-02-24 17:48:02 +0200

    Finished router setup

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,12 @@ import { Suspense, lazy } from 'react'; // normally imported
 // don't forget to run "npm install react-router-dom"
 // in older versions, Switch was used instead of Routes
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './HomePage.tsx';
 
 const UseStateExample = lazy(() => import('./Hooks/useState.tsx'));
 const UseEffectExample = lazy(() => import('./Hooks/useEffect.tsx'));
 const UseMemoExample = lazy(() => import('./Hooks/useMemo.tsx'));
 
+import Navbar from './Components/Navbar.tsx';
 const Card = lazy(() => import('./Components/card.tsx'));
 const PropsExample = lazy(() => import('./Components/props.tsx'));
 const ConditionalRenderingExample = lazy(() => import('./Components/conditionalRendering.tsx'));
@@ -20,63 +20,66 @@ const Button = lazy(() => import('./StylingOptions/StylingOptions.tsx'));
 
 function App() {
 	return (
-		<Router>
-			<Routes>
+		<>
+			<Router>
+				<Navbar />
+				<Routes>
 
-				{/* exact path used to avoid conflicts with other pages */}
-				<Route exact path="/" element={
-				<Suspense fallback={<div>Loading home page...</div>}>
-					<HomePage />
-				</Suspense>
-				}/>
-
-				<Route exact path="/use-state" element={
-					<Suspense fallback={<div>Loading useState...</div>}>
-						<UseStateExample />
+					{/* exact path used to avoid conflicts with other pages */}
+					<Route exact path="/" element={
+					<Suspense fallback={<div>Loading home page...</div>}>
+						<h1>Home page</h1>
 					</Suspense>
-				}/>
+					}/>
 
-				<Route exact path="/use-effect" element={
-					<Suspense fallback={<div>Loading useEffect...</div>}>
-						<UseEffectExample />
-					</Suspense>
-				}/>
+					<Route exact path="/use-state" element={
+						<Suspense fallback={<div>Loading useState...</div>}>
+							<UseStateExample />
+						</Suspense>
+					}/>
 
-				<Route exact path="/use-memo" element={
-					<Suspense fallback={<div>Loading useMemo...</div>}>
-						<UseMemoExample />
-					</Suspense>
-				}/>
+					<Route exact path="/use-effect" element={
+						<Suspense fallback={<div>Loading useEffect...</div>}>
+							<UseEffectExample />
+						</Suspense>
+					}/>
 
-				<Route exact path="/card-component" element={
-					<Suspense fallback={<div>Loading card component...</div>}>
-						<Card />
-					</Suspense>
-				}/>
+					<Route exact path="/use-memo" element={
+						<Suspense fallback={<div>Loading useMemo...</div>}>
+							<UseMemoExample />
+						</Suspense>
+					}/>
 
-				<Route exact path="/styling-options" element={
-					<Suspense fallback={<div>Loading styling options...</div>}>
-						<Button />
-					</Suspense>
-				}/>
+					<Route exact path="/card-component" element={
+						<Suspense fallback={<div>Loading card component...</div>}>
+							<Card />
+						</Suspense>
+					}/>
 
-				<Route exact path="/props" element={
-					<Suspense fallback={<div>Loading props...</div>}>
-						<PropsExample name="Jacques-Louis David" age={77} isFrench={true} />
-						<PropsExample name="John William Godward" age={61} isFrench={false} />
-						<PropsExample />
-					</Suspense>
-				}/>
+					<Route exact path="/styling-options" element={
+						<Suspense fallback={<div>Loading styling options...</div>}>
+							<Button />
+						</Suspense>
+					}/>
 
-				<Route exact path="/conditional-rendering" element={
-					<Suspense fallback={<div>Loading conditional rendering...</div>}>
-						<ConditionalRenderingExample isLoggedIn={true} username="great_gandering_swan1298" />
-						<ConditionalRenderingExample isLoggedIn={false} username="great_gandering_swan1298" />
-						<ConditionalRenderingExample />
-					</Suspense>
-				}/>
-			</Routes>
-		</Router>
+					<Route exact path="/props" element={
+						<Suspense fallback={<div>Loading props...</div>}>
+							<PropsExample name="Jacques-Louis David" age={77} isFrench={true} />
+							<PropsExample name="John William Godward" age={61} isFrench={false} />
+							<PropsExample />
+						</Suspense>
+					}/>
+
+					<Route exact path="/conditional-rendering" element={
+						<Suspense fallback={<div>Loading conditional rendering...</div>}>
+							<ConditionalRenderingExample isLoggedIn={true} username="great_gandering_swan1298" />
+							<ConditionalRenderingExample isLoggedIn={false} username="great_gandering_swan1298" />
+							<ConditionalRenderingExample />
+						</Suspense>
+					}/>
+				</Routes>
+			</Router>
+		</>
 	);
 }
 
