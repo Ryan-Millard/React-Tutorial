1ff1d9de7f5bc28e801c7aaf3080351c760ff622
Ryan Millard <millardryandevon@gmail.com>
2024-02-25 11:07:09 +0200

    styling and list filter

--- src/Components/listRendering.tsx
+++ src/Components/listRendering.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
 
+import './listRendering.css';
+
 class Fruit {
 	name: string;
 	calories: number;
@@ -33,29 +35,63 @@ const List: FC = () => {
 	fruits.sort((a, b) => b.calories - a.calories);
 	const descendingList = mapToHtmlList(fruits);
 
+	const lowCalFruit = mapToHtmlList(fruits.filter(fruit => fruit.calories < 100));
+
+	const highCalFruit = mapToHtmlList(fruits.filter(fruit => fruit.calories > 100));
+
 	return (
 		<>
-			<span>
-			<h1>Alphabetically Sorted</h1>
-				<ol>{alphabeticalList}</ol>
-			</span>
-			<span>
-				<h1>Reverse Alphabetically Sorted</h1>
-				<ol>{revAlphabeticalList}</ol>
-			</span>
-			<h1>Ascending According to Calories</h1>
-			<ol>{ascendingList}</ol>
-			<h1>Descending According to Calories</h1>
-			<ol>{descendingList}</ol>
+			<div className="listRenderer ">
+				<table>
+					<thead>
+						<tr>
+							<th>Alphabetical Order</th>
+							<th>Reverse Alphabetical Order</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>{alphabeticalList}</td>
+							<td>{revAlphabeticalList}</td>
+						</tr>
+					</tbody>
+					<thead>
+						<tr>
+							<th>Ascending calories</th>
+							<th>Descending calories</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>{ascendingList}</td>
+							<td>{descendingList}</td>
+						</tr>
+					</tbody>
+					<thead>
+						<tr>
+							<th>Low calories</th>
+							<th>High calories</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>{lowCalFruit}</td>
+							<td>{highCalFruit}</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 		</>
 	);
 }
 
 function mapToHtmlList(fruits: Fruit[]):JSX.Element {
 	return (
-		fruits.map(({ id, name, calories }):JSX.Element => (
-			 <li key={id}>{name}: &nbsp;{calories} calories</li>
-		))
+		<ol>{
+				fruits.map(({ id, name, calories }):JSX.Element => (
+							<li key={id}>{name}: &nbsp;{calories} calories</li>
+							))
+		}</ol>
 	);
 }
 
